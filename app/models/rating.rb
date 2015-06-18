29f1caa235945936d7b6c639eed5ebb8a778955d
cnmillar <cnmillar@gmail.com>
2015-06-18 20:42:51 +0000

    callbacks complete

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,18 +3,31 @@ class Rating < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :landlord
 
-	validates :communication, numericality: { minimum: 1, maximum: 5 }
-	validates :helpfulness, numericality: { minimum: 1, maximum: 5 }
-	validates :reliability, numericality: { minimum: 1, maximum: 5 }
+	validates :communication, presence: true, numericality: { minimum: 1, maximum: 5 }
+	validates :helpfulness, presence: true, numericality: { minimum: 1, maximum: 5 }
+	validates :reliability, presence: true, numericality: { minimum: 1, maximum: 5 }
 	validates :comment, length: { maximum: 800 }
 
-	after_create :update_landlord_average_rating
+	after_save :update_landlord_average_rating
+	after_create :update_friendly_icon
 
 	private
 
 	def update_landlord_average_rating
-		if communication && helpfulness && reliability
-			
+		all_landlord_ratings = Rating.where(landlord_id: landlord_id)
+		num_landlord_ratings = all_landlord_ratings.length
+		communication_indicator = all_landlord_ratings.sum(:communication).to_f / num_landlord_ratings
+		helpfulness_indicator = all_landlord_ratings.sum(:helpfulness).to_f / num_landlord_ratings
+		reliability_indicator = all_landlord_ratings.sum(:reliability).to_f / num_landlord_ratings
+		average_rating = (communication_indicator + helpfulness_indicator + reliability_indicator) / 3
+		landlord.average_rating = (average_rating * 2).round / 2.0
+		landlord.save
+	end
+
+	def update_friendly_icon
+		if !landlord.friendly && Rating.where(landlord_id: landlord_id).where(friendly: true).count(:friendly) >= 2
+			landlord.friendly = true
+			landlord.save
 		end
 	end
 
