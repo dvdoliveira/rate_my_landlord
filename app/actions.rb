e5da337422a66a8eb2ac343d54271bf7a0473f07
Deivide (David) Oliveira <deivide.s.oliveira@gmail.com>
2015-06-18 19:21:54 +0000

    Add a few routes files

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,45 @@
-# Homepage (Root path)
+# Homepage (Search box)
 get '/' do
   erb :index
 end
+
+# Login form
+get '/session/new' do
+  erb :'session/new'
+end
+
+# Login and redirects users to homepage
+post '/session' do
+  redirect '/'
+end
+
+# Sign up form
+get '/users/new' do
+  erb :'users/new'
+end
+
+# Sign-up and redirects users to homepage
+post '/users' do
+  redirect '/'
+end
+
+# Logout action and redirects logged out users to homepage
+delete '/session' do
+  redirect '/'
+end
+
+# Index of landlords
+get '/landlords' do
+  erb :'landlords/index'
+end
+
+get '/landlords/:id' do
+  erb :'landlords/show'
+end
+
+
+
+
+
+
+
